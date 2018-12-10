322006832a8f164fe06a58e3efe3182ebad2184f
Jerome Wei <jeromew@berkeley.edu>
2018-12-09 21:54:37 -0800

    Updates

---
+++ precache-manifest.7ff49df03d0dda8dd0cfc77065ea32d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "371948be93a7bcc1cbf0",
+    "url": "/drag-poetry/static/css/main.2d18e955.chunk.css"
+  },
+  {
+    "revision": "371948be93a7bcc1cbf0",
+    "url": "/drag-poetry/static/js/main.371948be.chunk.js"
+  },
+  {
+    "revision": "cc38a40c555cd275cd5b",
+    "url": "/drag-poetry/static/js/1.cc38a40c.chunk.js"
+  },
+  {
+    "revision": "aa58296dd0adc25307c3",
+    "url": "/drag-poetry/static/js/runtime~main.aa58296d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drag-poetry/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "59e1f59d0f818fcf9d2c17d993382c76",
+    "url": "/drag-poetry/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/drag-poetry/precache-manifest.7ff49df03d0dda8dd0cfc77065ea32d6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/drag-poetry/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
